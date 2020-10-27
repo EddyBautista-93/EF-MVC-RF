7a4ea1aa471ee1539148e4fc74e23e1d67731008
EddyBautista <bautista.j.eddy@gmail.com>
2020-10-26 22:33:12 -0500

    Added DAL

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EF_MVC_RF.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ContosoUniversity.Models;
+
+namespace EF_MVC_RF.DAL
+{
+    // inherits methods from dbDBcontext
+    public class SchoolContext : DbContext
+    {
+        // connection string passed through here
+        public SchoolContext() : base("SchoolContext")
+        {
+            //
+        }
+        // represents the entities in the context that can be queried in the db
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversity.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.EnterpriseServices;
+
+namespace EF_MVC_RF.Models
+{
+    public class Course
+    {
+        //tribute lets you enter the primary key for the course rather than having the database generate it.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        // navigation prop
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
